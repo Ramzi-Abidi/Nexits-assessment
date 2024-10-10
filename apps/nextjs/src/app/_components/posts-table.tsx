c4f7b82c3ff4677399e80bf7a95623096c20845b
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-10-10 21:31:31 +0100

    feat/posts-trpc: Run prettier

--- apps/nextjs/src/app/_components/posts-table.tsx
+++ apps/nextjs/src/app/_components/posts-table.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 import type { Post } from "@acme/db/schema";
 
+import type { getViews } from "../_lib/queries";
 import type { DataTableFilterField } from "~/types";
 import { DataTableAdvancedToolbar } from "~/components/data-table/advanced/data-table-advanced-toolbar";
 import { DataTable } from "~/components/data-table/data-table";
 import { TableInstanceProvider } from "~/components/data-table/table-instance-provider";
 import { useDataTable } from "~/hooks/use-data-table";
 import { api } from "~/trpc/react";
-import type { getViews } from "../_lib/queries";
 import { getColumns } from "./posts-table-columns";
 import { PostsTableFloatingBar } from "./posts-table-floating-bar";
 import { PostsTableToolbarActions } from "./posts-table-toolbar-actions";
